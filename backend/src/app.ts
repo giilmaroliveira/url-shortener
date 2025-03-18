d1dbdd59fb3fef1eef07ede0ad75d90da9c1d81a
Gilmar Oliveira <giilmaroliveira@outlook.com>
2025-03-17 23:27:08 -0300

    feat: add centralized error handling middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import urlRoutes from "./modules/url/routes";
+import errorHandler from "./middlewares/errorHandler";
 
 dotenv.config();
 
@@ -10,5 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api/url", urlRoutes);
+app.use(errorHandler);
 
 export default app;
