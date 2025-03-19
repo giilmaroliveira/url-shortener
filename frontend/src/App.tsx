c12748c7361665b8d2ff58f801028549374e5505
Gilmar Oliveira <giilmaroliveira@outlook.com>
2025-03-19 00:50:46 -0300

    feat: add navigation bar with links to Home and History pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,29 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { AppBar, Toolbar, Button } from "@mui/material";
 import StatsPage from "./pages/StatsPage";
 import UrlShortenerPage from "./pages/UrlShortenerPage";
 import UrlHistoryPage from "./pages/UrlHistoryPage";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<UrlShortenerPage />} />
-      <Route path="/stats/:slug" element={<StatsPage />} />
-      <Route path="/history" element={<UrlHistoryPage />} />
-    </Routes>
+    <>
+      <AppBar position="static" sx={{ mb: 4 }}>
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/history">
+            History
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Routes>
+        <Route path="/" element={<UrlShortenerPage />} />
+        <Route path="/stats/:slug" element={<StatsPage />} />
+        <Route path="/history" element={<UrlHistoryPage />} />
+      </Routes>
+    </>
   );
 }
 
