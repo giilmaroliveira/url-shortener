6d178334dd844ed4aac485a5f4187bafb0f2fdb5
Gilmar Oliveira <giilmaroliveira@outlook.com>
2025-03-18 22:50:20 -0300

    feat: implement URL shortening functionality in UrlShortenerForm component

--- frontend/src/components/UrlShortenerForm.tsx
+++ frontend/src/components/UrlShortenerForm.tsx
@@ -1,20 +1,61 @@
-import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import axios from "axios";
 
 const UrlShortenerForm = () => {
+  const [originalUrl, setOriginalUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+    setShortUrl("");
+
+    if (!originalUrl.trim()) {
+      setError("Please enter a valid URL.");
+      return;
+    }
+
+    try {
+      const response = await axios.post("http://localhost:3000/api/url", {
+        originalUrl
+      });
+      setShortUrl(response.data.shortUrl.slug);
+    } catch (err) {
+      setError("Failed to shorten the URL. Please try again.");
+    }
+  };
+
+  const handleCopy = () => {
+
+  }
   return (
     <Box mt={4} textAlign="left">
-      <form>
+      <form onSubmit={handleSubmit}>
         <TextField
           label="Enter URL"
           variant="outlined"
           fullWidth
-          value=""
+          value={originalUrl}
+          onChange={(e) => setOriginalUrl(e.target.value)}
+          error={!!error}
+          helperText={error}
           sx={{ mb: 2 }}
         />
+        <Button type="submit" variant="contained" color="primary">
+          Shorten
+        </Button>
       </form>
-      <Button type="submit" variant="contained" color="primary">
-        Shorten
-      </Button>
+
+      {shortUrl && (
+        <Box mt={3}>
+          <Typography variant="h6" color="success">
+            Success! Here's your short URL
+          </Typography>
+          <Typography variant="body1">{`http://localhost:3000/api/url/${shortUrl}`}</Typography>
+        </Box>
+      )}
     </Box>
   )
 }
